c7b91e99e1cad2781b11c250ced8bc4d51d0a460
John Chappelle <jrchap82@gmail.com>
2018-11-06 11:13:26 -0500

    feat:  enable markdown parsing

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,13 @@
 module.exports = {
-	plugins: [`gatsby-plugin-netlify-cms`],
-}
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/blog}`,
+        name: 'markdown-pages',
+      },
+    },
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-transformer-remark`,
+  ],
+};
